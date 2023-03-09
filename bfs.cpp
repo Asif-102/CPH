90aa9b4b29804f30d36268e63ff1e734e9c0a4fb
Asif-102 <asif1zero2@gmail.com>
2023-03-10 00:26:26 +0600

    traverse with level

--- bfs.cpp
+++ bfs.cpp
@@ -8,20 +8,24 @@ using namespace std;
 const int N = 1e5;
 
 int visited[N];
+int level[N];
+
 vector<int> adj_list[N];
 
-void bfs(int src){
+void bfs(int src) {
     queue<int> q;
-    visited[src] = 1;
     q.push(src);
-    while(!q.empty()){
+    visited[src] = 1;
+    level[src] = 0;
+    while (!q.empty()) {
         int head = q.front();
         q.pop();
-        cout << head << ' ';
-        for(int adj_node: adj_list[head]){
-            if(visited[adj_node] == 0){
-                visited[adj_node] = 1;
+        cout << "(" << head << "," << level[head] << ")" << ' ';
+        for (int adj_node : adj_list[head]) {
+            if (visited[adj_node] == 0) {
                 q.push(adj_node);
+                level[adj_node] = level[head] + 1;
+                visited[adj_node] = 1;
             }
         }
     }
