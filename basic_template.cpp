1ae50787580ae1ed752194c61d35734f8bf8e4b5
Asif-102 <asif1zero2@gmail.com>
2023-03-16 01:30:13 +0600

    update template

---
+++ basic_template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define long long long int
+#define DEBUG
+using namespace std;
+
+// @author: Asif_102
+
+struct test {
+    void solve() {
+        
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+
+    int tc;
+    cin >> tc;
+    for (int i = 0; i < tc; i++) {
+        test().solve();
+    }
+
+    return 0;
+}
